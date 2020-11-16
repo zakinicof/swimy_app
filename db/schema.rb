f5286e6bfae73e131aa36c20fa7e0b78f8e87b96
zakinicof <72780750+zakinicof@users.noreply.github.com>
2020-11-16 12:16:25 +0900

    ユーザー一覧表示機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_095004) do
+ActiveRecord::Schema.define(version: 2020_11_16_002111) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_11_13_095004) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
