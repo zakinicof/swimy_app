d3d5e4c191b898cf9af3b58b256500df69ab24a9
zakinicof <72780750+zakinicof@users.noreply.github.com>
2020-12-04 20:48:39 +0900

    修正

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -11,7 +11,7 @@ class LessonsController < ApplicationController
       clear_count = LessonUser.where(user_id: @user.id, lesson_id: lesson.id).count
       clear = clear_count.to_f / item_count * 100
       yet = 100 - clear
-      progress = {'達成' => clear.round(1), '未達成' => yet.round(1)}
+      progress = {'達成' => clear.round, '未達成' => yet.round}
       @progresses << progress
     end
   end
