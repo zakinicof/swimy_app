c85806908104a2a9bb3588a5b9823ac9ff6f1486
zakinicof <72780750+zakinicof@users.noreply.github.com>
2020-12-06 20:41:22 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :admin])
     devise_parameter_sanitizer.permit(:sign_in, keys:[:email, :password])
   end
+  
 end
